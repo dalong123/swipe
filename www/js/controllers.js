1a81a587ceeab975cedc5db2401b6eb26ddc9683
Phillip Curl <phillipcurl@gmail.com>
2015-11-08 12:34:45 -0500

    Blowing away changes from last commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,8 +11,6 @@ angular.module('starter.controllers', [])
 *   guarantees that the only time we're pulling down data are when the applicable
 *   view is being rendered. The call to the service should also be the very
 *   first thing each controller does if it is pulling down data from a service.
-*   I set up the PostsCtrl as an example of some best practices I think we
-*   should follow
 */
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
@@ -52,7 +50,7 @@ angular.module('starter.controllers', [])
 /**
  *
  */
-.controller('BlogsCtrl', function($scope, $q, LocalStorage, Blog) {
+.controller('BlogsCtrl', function($scope, $q, Blog, LocalStorage) {
 
   // This is the ionic-specific funtion used to target the view's entry. As a
   // result of template caching, this controller is only called when one of its
@@ -64,7 +62,7 @@ angular.module('starter.controllers', [])
 
     if(!angular.equals({}, blogsLocalStore))
     {
-      return blogsLocalStore;
+      $scope.blogs = blogsLocalStore;
     }
     else {
       Blog.getBlogsAsync().then(
@@ -72,6 +70,7 @@ angular.module('starter.controllers', [])
           // promise was fullfilled (regardless of outcome)
           // checks for information will be peformed here
           $scope.blogs = result;
+          LocalStorage.setObject('blogs', result);
         },
         function(error) {
           // handle errors here
@@ -84,39 +83,100 @@ angular.module('starter.controllers', [])
 
 // We need to figure out a way to pass the entire blog object via the router
 // rather than fetching all of blogs.json and doing id filtering on it.
-.controller('BlogCtrl', function($scope, $stateParams, $filter, Blog, LocalStorage){
+.controller('BlogCtrl', function($scope, $stateParams, $filter, $ionicSwipeCardDelegate, $ionicModal, Blog, LocalStorage){
+
+  var blogId = $stateParams.blogId;
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/about-modal.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
 
   $scope.$on('$ionicView.enter', function(e) {
 
-  // take in the route param for the specific view (IT SHOULD BE A NUMBER)
-  var blogId = $stateParams.blogId;
-
-  if(blogId === 'all'){
-    // Build out our blog object for the all blogs view. We need to apply scope
-    // variables for everything used in the view
-    $scope.blog = {
-
+    // take in the route param for the specific view (IT SHOULD BE A NUMBER)
+    if(blogId === 'all'){
+      // Build out our blog object for the all blogs view. We need to apply scope
+      // variables for everything used in the view
+      $scope.blog = {
+        "title": "boobs"
+      }
     }
-  }
-  else
-  {
-    var blogsLocalStore = LocalStorage.getObject('blogs');
-
-    if(!angular.equals({}, blogsLocalStore))
+    else
     {
-      $scope.blog = $filter('filter')(blogsLocalStore, {id:blogId})[0];
-    } else {
-      Blog.getBlogsAsync().then(
-        function(result) {
-          // promise was fullfilled (regardless of outcome)
-          $scope.blog = $filter('filter')(result, {id:blogId})[0];
-        },
-        function(error) {
-          // handle errors here
-          console.log(error.statusText);
-        }
-      );
+      var blogLocalStore = LocalStorage.getObject('blog' + blogId);
+
+      if(!angular.equals({}, blogLocalStore))
+      {
+        $scope.blog = blogLocalStore;
+        $scope.cards = LocalStorage.getObject('blogs');
+      } else {
+        Blog.getBlogsAsync().then(
+          function(result) {
+            // promise was fullfilled (regardless of outcome)
+            $scope.blog = $filter('filter')(result, {id:blogId})[0];
+            $scope.cards = LocalStorage.getObject('blogs');
+            LocalStorage.setObject('blog' + blogId, $scope.blog);
+          },
+          function(error) {
+            // handle errors here
+            console.log(error.statusText);
+          }
+        );
+      }
     }
-  }
   });
+
+  // Open the login modal
+  $scope.showAbout = function() {
+    $scope.modal.show();
+  };
+  // Open the login modal
+  $scope.closeAbout = function() {
+    $scope.modal.hide();
+  };
 })
+
+.controller('CardsCtrl', function($scope, $ionicSwipeCardDelegate) {
+  var cardTypes = [{
+    title: 'Swipe down to clear the card',
+    image: 'img/pic.png'
+  }, {
+    title: 'Where is this?',
+    image: 'img/pic.png'
+  }, {
+    title: 'What kind of grass is this?',
+    image: 'img/pic2.png'
+  }, {
+    title: 'What beach is this?',
+    image: 'img/pic3.png'
+  }, {
+    title: 'What kind of clouds are these?',
+    image: 'img/pic4.png'
+  }];
+
+  $scope.cards = Array.prototype.slice.call(cardTypes, 0, 0);
+
+  $scope.cardSwiped = function(index) {
+    $scope.addCard();
+  };
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.addCard = function() {
+    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    newCard.id = Math.random();
+    $scope.cards.push(angular.extend({}, newCard));
+  }
+})
+
+.controller('CardCtrl', function($scope, $ionicSwipeCardDelegate) {
+  $scope.goAway = function() {
+    var card = $ionicSwipeCardDelegate.getSwipeableCard($scope);
+    card.swipe();
+  };
+});
--- www/js/services.js
+++ www/js/services.js
@@ -20,49 +20,11 @@ angular.module('starter.services', [])
 /**
  *
  */
-.factory('Blog', function($http, $q, $filter, LocalStorage) {
+.factory('Blog', function($http, $q, $filter) {
 
-  /**
-   * [defer description]
-   * @method defer
-   * @return {[type]} [description]
-   */
   var deferred = $q.defer();
 
   return {
-
-    /**
-     * [function description]
-     * @method function
-     * @return {[type]} [description]
-     */
-    getBlogs: function() {
-      // Get the blogs object from localStorage, regardless of whether it's
-      // defined or not
-      var blogsLocalStore = LocalStorage.getObject('blogs');
-      if(blogsLocalStore !== undefined){
-        return blogsLocalStore;
-      } else {
-        getBlogsAsync().then(
-          function(result) {
-            // promise was fullfilled (regardless of outcome)
-            // checks for information will be peformed here
-            LocalStorage.setObject('blogs', result);
-            return result;
-          },
-          function(error) {
-            // handle errors here
-            console.log(error.statusText);
-          }
-        );
-      }
-    },
-
-    /**
-     * [function description]
-     * @method function
-     * @return {[type]} [description]
-     */
     getBlogsAsync: function() {
       return $http.get('blogs.json')
         .then(function(response) {
@@ -75,14 +37,6 @@ angular.module('starter.services', [])
           return deferred.promise;
         });
     },
-
-    /**
-     * [function description]
-     * @method function
-     * @param  {[type]}  kimonoId   [description]
-     * @param  {Boolean} isOnDemand [description]
-     * @return {[type]}             [description]
-     */
     getFeedAsync: function(kimonoId, isOnDemand) {
       return $http.get('blogs.json')
         .then(function(response) {
