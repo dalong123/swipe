827dc1da7d1b054221536cfae3b93c30d9222e51
Phillip Curl <phillipcurl@gmail.com>
2015-11-08 22:38:32 -0500

    Adding iFrame support to modals
    We can now view blog posts in-app via modals

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -47,6 +47,27 @@ angular.module('starter.controllers', [])
   };
 })
 
+.controller('ModalCtrl', function($scope, $ionicModal) {
+
+  $ionicModal.fromTemplateUrl('templates/post-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+
+})
+
 /**
  *
  */
@@ -86,7 +107,7 @@ angular.module('starter.controllers', [])
 .controller('BlogCtrl', function($scope, $stateParams, $filter, $ionicSwipeCardDelegate, $ionicModal, Blog, LocalStorage){
 
   // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/about-modal.html', {
+  $ionicModal.fromTemplateUrl('templates/post-modal.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
