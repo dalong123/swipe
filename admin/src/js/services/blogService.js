c27037d555c73310e6cc0e5f1f462a110bdb9cc9
Steven Coon <scoon@hmbnet.com>
2015-11-19 08:36:09 -0500

    Deleting old http service methods

--- admin/src/js/services/blogService.js
+++ admin/src/js/services/blogService.js
@@ -11,44 +11,4 @@ function BlogService($q,$http,$resource){
     }),
     blog: $resource('http://localhost:8888/api/blogs')
   }
-
-  var BlogService = {
-          //arrays
-
-          //methods
-          CreateBlog: _create,
-          GetAllBlogs: _all,
-          UpdateBlog: _update,
-          GetBlog: _get,
-          DeleteBlog: _delete
-      };
-
-  return BlogService;
-
-  // get a single blog
-  function _get(id) {
-    return $http.get('http://localhost:8888/api/blogs/' + id);
-  };
-
-  // get all blogs
-  function _all() {
-    return $http.get('http://localhost:8888/api/blogs/');
-  };
-
-  // create a blog
-  function _create(userData) {
-    return $http.post('http://localhost:8888/api/blogs/', userData);
-  };
-
-  // update a blog
-
-  function _update(id, userData) {
-    return $http.put('http://localhost:8888/api/blogs/' + id, userData);
-  };
-
-  // delete a blog
-  function _delete(id) {
-    return $http.delete('http://localhost:8888/api/blogs/' + id);
-  };
-
 };
