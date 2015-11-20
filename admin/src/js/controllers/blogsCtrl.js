ccd70949c9c3a24d6a12f44787dc3d9a43dcec7f
Steven Coon <scoon@hmbnet.com>
2015-11-19 19:00:28 -0500

    Refactored blogs
    -it won't look like this, there will be edit buttons for the href, but
    this is the jist
    - state routing isn't set up for this route, yet

--- admin/src/js/controllers/blogsCtrl.js
+++ admin/src/js/controllers/blogsCtrl.js
@@ -3,14 +3,14 @@ angular.module('SwipeAdmin').controller('BlogsCtrl', BlogsCtrl);
 BlogsCtrl.$inject = ['$scope','BlogService'];
 
 function BlogsCtrl($scope, BlogService){
-  $scope.blog = {};
+  // $scope.blog = {};
   $scope.currentBlogs = [];
-  $scope.currentBlog = {};
+  // $scope.currentBlog = {};
 
   $scope.currentBlogs = BlogService.blog.query();
-  $scope.currentBlog = $scope.currentBlogs[0];
-  console.log($scope.currentBlogs);
-  console.log($scope.currentBlog);
+  // $scope.currentBlog = $scope.currentBlogs[0];
+  // console.log($scope.currentBlogs);
+  // console.log($scope.currentBlog);
 
 
 
@@ -18,33 +18,33 @@ function BlogsCtrl($scope, BlogService){
   //   $scope.currentBlogs = result.data;
   //   $scope.currentBlog = result.data[0];
   // });
-
-  $scope.AddBlog = function(){
-    BlogService.blog.save($scope.blog,
-                   function(data) {
-                      alert("Saved!");
-                      $scope.curator = {};
-                   }, function(e) {
-                      // failure
-                      alert("Error");
-                   });
-    // BlogService.CreateBlog($scope.blog).then(function(result){
-    //   $scope.blog = {};
-    //   alert("Added!");
-    // });
-  }
-
-  $scope.UpdateBlog = function(blog){
-    BlogService.blogs.update({blog_id: blog._id},blog,
-                   function(data) {
-                      alert("Saved!");
-
-                   }, function(e) {
-                      // failure
-                      alert("Error");
-                   });
-    // BlogService.UpdateBlog(blog._id).then(function(result){
-    //   alert("Updated!");
-    // });
-  }
+  //
+  // $scope.AddBlog = function(){
+  //   BlogService.blog.save($scope.blog,
+  //                  function(data) {
+  //                     alert("Saved!");
+  //                     $scope.curator = {};
+  //                  }, function(e) {
+  //                     // failure
+  //                     alert("Error");
+  //                  });
+  //   // BlogService.CreateBlog($scope.blog).then(function(result){
+  //   //   $scope.blog = {};
+  //   //   alert("Added!");
+  //   // });
+  // }
+  //
+  // $scope.UpdateBlog = function(blog){
+  //   BlogService.blogs.update({blog_id: blog._id},blog,
+  //                  function(data) {
+  //                     alert("Saved!");
+  //
+  //                  }, function(e) {
+  //                     // failure
+  //                     alert("Error");
+  //                  });
+  //   // BlogService.UpdateBlog(blog._id).then(function(result){
+  //   //   alert("Updated!");
+  //   // });
+  // }
 };
--- admin/src/js/controllers/curatorCtrl.js
+++ admin/src/js/controllers/curatorCtrl.js
@@ -4,29 +4,29 @@ CuratorCtrl.$inject = ['$scope','CuratorService'];
 
 function CuratorCtrl($scope, CuratorService){
 
-  $scope.currentCurators = [];
-  $scope.curator = {};
-  $scope.curator.songs = [];
-
-  $scope.currentCurators = CuratorService.curators.query();
-
-  $scope.AddCurator = function(){
-
-    $scope.curator.songs.push({url:$scope.curator.newSongs});
-    console.log($scope.curator);
-    CuratorService.curators.save($scope.curator,
-                   function(data) {
-                      alert("Saved!");
-                      $scope.curator = {};
-                   }, function(e) {
-                      // failure
-                      alert("Error");
-                   });
-
-  };
-
-  $scope.UpdateCurator = function($scope.currentCurator){
-
-  };
+  // $scope.currentCurators = [];
+  // $scope.curator = {};
+  // $scope.curator.songs = [];
+  //
+  // $scope.currentCurators = CuratorService.curators.query();
+  //
+  // $scope.AddCurator = function(){
+  //
+  //   $scope.curator.songs.push({url:$scope.curator.newSongs});
+  //   console.log($scope.curator);
+  //   CuratorService.curators.save($scope.curator,
+  //                  function(data) {
+  //                     alert("Saved!");
+  //                     $scope.curator = {};
+  //                  }, function(e) {
+  //                     // failure
+  //                     alert("Error");
+  //                  });
+  //
+  // };
+  //
+  // $scope.UpdateCurator = function($scope.currentCurator){
+  //
+  // };
 
 };
