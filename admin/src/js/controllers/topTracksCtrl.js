c787076e25ba0b65f0f403c50fb3fe3266ad28de
Phillip Curl <phillipcurl@gmail.com>
2015-11-22 15:11:01 -0500

    Making some style changes to genres and toptracks
    Also added a confirm dialog when deleting a song from top tracks

--- admin/src/js/controllers/topTracksCtrl.js
+++ admin/src/js/controllers/topTracksCtrl.js
@@ -1,24 +1,27 @@
 angular.module('SwipeAdmin').controller('TopTracksCtrl', TopTracksCtrl);
 
-TopTracksCtrl.$inject = ['$scope','TopTracksService'];
+TopTracksCtrl.$inject = ['$scope', 'TopTracksService'];
 
-function TopTracksCtrl($scope, TopTracksService){
+function TopTracksCtrl($scope, TopTracksService) {
 
-  TopTracksService.GetAllTopTracks().then(function(result){
-      $scope.topTrack = result.data[0];
+  TopTracksService.GetAllTopTracks().then(function(result) {
+    $scope.topTrack = result.data[0];
   });
 
-  $scope.AddSong = function(song){
+  $scope.AddSong = function(song) {
     $scope.topTrack.songs.push(song);
     $scope.newSong = {};
   }
 
-  $scope.RemoveSong = function(songIndex){
-        $scope.topTrack.songs.splice(songIndex, 1);
+  $scope.RemoveSong = function(songIndex) {
+    var confirmDelete = confirm("Are you sure you want to delete this? You won't be able to get it back!");
+    if (confirmDelete === true) {
+      $scope.topTrack.songs.splice(songIndex, 1);
+    }
   }
 
-  $scope.SaveTopTracks= function() {
-    TopTracksService.UpdateTrack($scope.topTrack._id,$scope.topTrack).then(function(result){
+  $scope.SaveTopTracks = function() {
+    TopTracksService.UpdateTrack($scope.topTrack._id, $scope.topTrack).then(function(result) {
       alert("Top Tracks Saved!");
     });
   }
